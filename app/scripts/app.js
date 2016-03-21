5c6c8e35acc3965c5c524d09169fdad14c1c0440
mcabe <mcabe@me.com>
2016-03-21 21:16:52 +0100

    initial

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,21 +15,52 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ui.router'
   ])
-  .config(function ($routeProvider) {
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'views/main.html'
+      })
+      .state('offers', {
+        url: '/tilbud',
+        templateUrl: 'views/offers.html'
+      })
+      .state('about', {
+        url: '/om',
+        templateUrl: 'views/about.html'
+      })
+      .state('publications', {
+        url: '/publikationer',
+        templateUrl: 'views/publications.html'
+      });
+  });
+  /*.config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
+      .when('/offers', {
+        templateUrl: 'views/offers.html',
+        controller: 'OffersCtrl',
+        controllerAs: 'offers'
+      })
       .when('/about', {
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl',
         controllerAs: 'about'
       })
+      .when('publications',{
+        templateUrl: 'views/publications.html',
+        controller: 'PublicationsCtrl',
+        controllerAs: 'publications'
+      })
       .otherwise({
         redirectTo: '/'
       });
-  });
+  });*/
---
+++ app/scripts/controllers/offers.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Kris on 3/21/2016.
+ */
